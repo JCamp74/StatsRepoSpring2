38904f6d8886da0ee38580fbaeee009dbdf00258
JCamp74 <jbcampbell10@yahoo.com>
2025-05-06 00:49:44 -0400

    Update plotter.m to include labels.

--- PlotterSalterSmootherProject/MatLab PlotterSalterSmoother/plotter.m
+++ PlotterSalterSmootherProject/MatLab PlotterSalterSmoother/plotter.m
@@ -6,7 +6,7 @@ clf; %clearing graph to use multiple times.
 hold on; %holding so all three graphs can show at once.
 
 %plotting the plotter with blue color, 2 thickness
-plot(x, y, 'b-', 'LineWidth', 2);
+plot(x, y, 'b-', 'DisplayName', 'Original Data', 'LineWidth', 1.5);
 
 
 %loop to salt the data comparatively to the java file.
@@ -14,7 +14,7 @@ for i = 1:length(y)
   temp = y(i);
   temp = temp + (rand() * 200 - 100);
   y(i) = temp;
-end;
+endfor;
 
 
 
@@ -25,7 +25,11 @@ smoother = movmean(y, winSize);
 %plotting the salted and smoothed graphs.
 %salted is purple with thickness 2.
 %smoothed is red with thickness 2.
-plot(x, y, 'Color', '#800080', 'LineWidth', 2);
-plot(x, smoother, 'r-', 'LineWidth', 2);
+plot(x, y, 'Color', '#800080', 'LineWidth', 1.5, 'DisplayName', 'Salted Data');
+plot(x, smoother, 'r-', 'LineWidth', 1.5, 'DisplayName', 'Smoothed Data');
+legend;
+title('Plotted, Salted and Smoothed Data from function y = x^2 + 2');
+xlabel('Index');
+ylabel('Y Values');
 hold off; %stop the hold on graph display.
 
